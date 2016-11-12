15a5cfe9723727a4590abf58fcb0254622027a15
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-11-12 17:58:56 +0100

    Add a code coverage target

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -9,6 +9,7 @@ else
     sudo add-apt-repository ppa:beineri/opt-qt57-trusty -y
     sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
     sudo apt-get update -q
-    sudo apt-get install cmake qt57base qt57declarative qt57quickcontrols2 -y
+    sudo apt-get install cmake qt57base qt57declarative qt57quickcontrols2 lcov -y
+    gem install coveralls-lcov
     source /opt/qt57/bin/qt57-env.sh
 fi
