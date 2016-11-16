1ce00fce486b82c427aceb6d88d60858bccb1534
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-11-16 19:47:00 +0100

    Log4Cplus: add missing integration test for qt message handler
    and refactor other integration test to use the logging manager.

--- common/logging/tests/TestLog4CplusLogger.cpp
+++ common/logging/tests/TestLog4CplusLogger.cpp
@@ -3,69 +3,71 @@
 #include <hackedit/common/logging/ILogger.hpp>
 #include <hackedit/common/logging/log4cplus/Log4CplusLoggerFactory.hpp>
 #include <hackedit/common/Cpp14Support.hpp>
+#include <QDebug>
+#include <hackedit/common/logging/LoggingManager.hpp>
 
 USE_HACKEDIT_NAMESPACE2(Common, Logging)
 
 class TestLog4CplusLogger: public testing::Test {
 public:
     static void SetUpTestCase() {
-        factory = std::move(Log4CplusLoggerFactory::basicConfig());
+        LoggingManager::initialize(Log4CplusLoggerFactory::basicConfig(), true);
     }
-    static void TearDownTestCase() {
-        factory = nullptr;
-    }
-
-protected:
-    static std::unique_ptr<ILoggerFactory> factory;
 };
 
-std::unique_ptr<ILoggerFactory> TestLog4CplusLogger::factory = nullptr;
 
-
-TEST_F(TestLog4CplusLogger, BasicConfig_TRACE_NoDisplay_OnStdout) {
-    auto logger = factory->logger();
+TEST_F(TestLog4CplusLogger, BasicConfig_TRACE_NoDisplay_OnStdout_IntegrationTest) {
+    auto logger = LoggingManager::logger();
     testing::internal::CaptureStdout();
     logger->log("A message", LogLevel::Trace, __FILE__, __LINE__);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "");
 }
 
-TEST_F(TestLog4CplusLogger, BasicConfig_DEBUG_Display_OnStdout) {
-    auto logger = factory->logger();
+TEST_F(TestLog4CplusLogger, BasicConfig_DEBUG_Display_OnStdout_IntegrationTest) {
+    auto logger = LoggingManager::logger();
     testing::internal::CaptureStdout();
     logger->log("A message", LogLevel::Debug, __FILE__, __LINE__);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "DEBUG - A message\n");
 }
 
-TEST_F(TestLog4CplusLogger, BasicConfig_INFO_Display_OnStdout) {
-    auto logger = factory->logger();
+TEST_F(TestLog4CplusLogger, BasicConfig_INFO_Display_OnStdout_IntegrationTest) {
+    auto logger = LoggingManager::logger();
     testing::internal::CaptureStdout();
     logger->log("A message", LogLevel::Info, __FILE__, __LINE__);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "INFO - A message\n");
 }
 
-TEST_F(TestLog4CplusLogger, BasicConfig_WARNING_Display_OnStdout) {
-    auto logger = factory->logger();
+TEST_F(TestLog4CplusLogger, BasicConfig_WARNING_Display_OnStdout_IntegrationTest) {
+    auto logger = LoggingManager::logger();
     testing::internal::CaptureStdout();
     logger->log("A message", LogLevel::Warning, __FILE__, __LINE__);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "WARN - A message\n");
 }
 
-TEST_F(TestLog4CplusLogger, BasicConfig_ERROR_Display_OnStdout) {
-    auto logger = factory->logger();
+TEST_F(TestLog4CplusLogger, BasicConfig_ERROR_Display_OnStdout_IntegrationTest) {
+    auto logger = LoggingManager::logger();
     testing::internal::CaptureStdout();
     logger->log("A message", LogLevel::Error, __FILE__, __LINE__);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "ERROR - A message\n");
 }
 
-TEST_F(TestLog4CplusLogger, BasicConfig_FATAL_Display_OnStdout) {
-    auto logger = factory->logger();
+TEST_F(TestLog4CplusLogger, BasicConfig_FATAL_Display_OnStdout_IntegrationTest) {
+    auto logger = LoggingManager::logger();
     testing::internal::CaptureStdout();
     logger->log("A message", LogLevel::Fatal, __FILE__, __LINE__);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "FATAL - A message\n");
+}
+
+TEST_F(TestLog4CplusLogger, qWarning_Display_OnStdout_IntegrationTest) {
+    auto logger = LoggingManager::logger();
+    testing::internal::CaptureStdout();
+    qWarning() << "test";
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "WARN - test\n");
 }
